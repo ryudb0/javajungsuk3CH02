8c011932a37d21daafd922d0caaf388b00f339a9
ryudb0 <ryudb030@gmail.com>
2023-01-18 16:23:31 +0900

    git test

--- src/Ex2_1.java
+++ src/Ex2_1.java
@@ -6,5 +6,11 @@ public class Ex2_1 {
 		
 		System.out.println(year);
 		System.out.println(age);
+		
+		year = age + 2000;
+		age = age + 1;
+		
+		System.out.println(year);
+		System.out.println(age);
 	}
 }
